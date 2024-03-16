51c22f3453f878b1a400469b2221c767de170eeb
Prathamesh Andhare <92220219+PrathameshAndhare@users.noreply.github.com>
2024-03-16 12:18:35 +0530

    Update followersContext.jsx

--- src/context/followersContext.jsx
+++ src/context/followersContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useState } from 'react';
 export const FollowersContext = createContext();
 
 export const FollowersProvider = ({ children }) => {
-	const [unfollowing, SetUnfollowing] = useState(); 
+	const [unfollowing, SetUnfollowing] = useState();
 
 	return (
 		<FollowersContext.Provider value={{ unfollowing, SetUnfollowing }}>
@@ -16,3 +16,12 @@ export const FollowersProvider = ({ children }) => {
 FollowersProvider.propTypes = {
 	children: PropTypes.node.isRequired,
 };
+// FollowersProvider, you must pass at least one child component or element to it. If you don't provide any children, React will issue a warning in development mode.
+
+// FollowersProvider creates a special container that holds important information (whether a user is unfollowing something) and allows other parts of your app to access and use this information as needed.
+
+// Instead of passing props down everywhere, a space is created by createContext()
+// context -> space 
+// To store data, .Provider is used which takes arg as value which denotes the data to be stored.
+
+// This box (FollowersProvider) wraps around other parts of your application. It tells React that any component inside it can access the unfollowing information and the tool SetUnfollowing.
