d3f77c2d46eed37330c7142f8e14f568bc934602
Prathamesh Andhare <92220219+PrathameshAndhare@users.noreply.github.com>
2024-03-16 12:20:30 +0530

    Update cardStyle.css.js

--- src/styles/cardStyle.css.js
+++ src/styles/cardStyle.css.js
@@ -1,33 +1,46 @@
 import styled from 'styled-components';
 
 export const CardStyle = styled.div`
-	width: 100px;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	padding: 5px 0;
-	margin: 1rem 0;
-	border-radius: 10px;
-	.white {
-		background-color: var(--white);
-	}
-	.whitesmoke {
-		background-color: whitesmoke;
-	}
-	p {
-		margin: 0.3rem 0;
-		font-size: 15px;
-		width: 100px;
-		word-wrap: break-word;
-	}
-	img {
-		width: 80px;
-		height: 80px;
-		border-radius: 50%;
-	}
-	a img {
-		width: 15px;
-		height: 15px;
-		border-radius: 0;
-	}
+    width: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 5px 0;
+    margin: 1rem 0;
+    border-radius: 10px;
+    background-color: lightblue;
+    font-size: 15px;
+    white-space: normal;
+    text-align: center;
+    word-wrap: break-word;
+    transition: transform 0.2s ease;
+    border: 2px solid lightpink; 
+    .white {
+        background-color: lightblue;
+    }
+    .whitesmoke {
+        background-color: lightcoral;
+    }
+    p {
+        margin: 0.3rem 0;
+        font-size: 15px;
+        width: 100px;
+        word-wrap: break-word;
+    }
+    
+    &:hover {
+        transform: scale(1.1); /* Enlarge the card on hover */
+    }
+
+    img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+
+    a img {
+        width: 15px;
+        height: 15px;
+        border-radius: 0;
+    }
 `;
