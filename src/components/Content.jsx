673a013301d1ef203838cf3841a696f7d79e0bd8
Prathamesh Andhare <92220219+PrathameshAndhare@users.noreply.github.com>
2024-03-16 12:16:24 +0530

    Update Content.jsx

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,24 +4,20 @@ export default function Content() {
 	return (
 		<ContentStyle>
 			<section>
-				<p>Check who is unfollowed you and who you do not follow back</p>
-			</section>
-			<section>
 				<h4>How does it work?</h4>
 				<p>
-					Pretty simple. just type your GitHub user in the search box and hit
-					search
-				</p> 
+					Type GitHub username and click search.
+				</p>
 			</section>
 			<section>
 				<p>
-					The code for this project is{' '}
+					The code for this project is avilable on {' '}
 					<a
-						href='https://github.com/CsarGomez/github-unfollow-tracker'
+						href='https://github.com/PrathameshAndhare/unfollow-tracker.git'
 						target='_blank'
 						rel='noreferrer'
 					>
-						available on GitHub
+						GitHub
 					</a>
 				</p>
 			</section>
